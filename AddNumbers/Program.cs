7dc816db1765d0c40c30ddd796bdc70c8b9b7f9e
Student <Student@DESKTOP-9LDA21V>
2022-11-17 14:44:55 -0500

    Add project files.

---
+++ AddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿/*This program returns the sum of two integers from the user*/
+
+using System;
+using static System.Console;
+class InteractiveAddition
+{
+    static void Main()
+    {
+        string userName, firstIntegerInputString, secondIntegerInputString;
+        int firstIntegerInput, secondIntegerInput, sumOfInputIntegers;
+
+        Write("Enter your name: ");
+        userName = ReadLine();
+        //Prompt for user name
+
+        Write("Hello {0}! Enter the first integer: ", userName);
+        firstIntegerInputString = ReadLine();
+        firstIntegerInput = Convert.ToInt32(firstIntegerInputString);
+
+        Write("Enter the second integer: ");
+        secondIntegerInputString = ReadLine();
+        secondIntegerInput = Convert.ToInt32(secondIntegerInputString);
+        //Prompt for inputs and convert strings to integers
+
+        sumOfInputIntegers = firstIntegerInput + secondIntegerInput;
+        WriteLine("{0}, the sum of {1} and {2} is {3}", userName, firstIntegerInput, secondIntegerInput, sumOfInputIntegers);
+        //Calculate and output result
+    }
+}
