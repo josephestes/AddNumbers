df1f5c9c5e8c3f9729df7b5f07b2f35d26a87e5e
Joseph <54252110+josephestes@users.noreply.github.com>
2022-12-07 22:11:41 -0500

    Optimize Program.cs
    This optimization uses the int.Parse() method to convert strings to integers instead of the Convert.ToInt32() method, which is a bit more concise. In addition, the string.Format() method is replaced with string interpolation for improved readability. Also, the variables userName, firstIntegerInputString, and secondIntegerInputString are no longer necessary, so they are removed. Finally, I have used the $ symbol before the string literals to indicate that they are interpolated strings.

--- AddNumbers/Program.cs
+++ AddNumbers/Program.cs
@@ -2,28 +2,24 @@
 
 using System;
 using static System.Console;
+
 class InteractiveAddition
 {
     static void Main()
     {
-        string userName, firstIntegerInputString, secondIntegerInputString;
-        int firstIntegerInput, secondIntegerInput, sumOfInputIntegers;
-
-        //Prompt for user name
+        // Prompt for user name
         Write("Enter your name: ");
-        userName = ReadLine();
+        string userName = ReadLine();
 
-        //Prompt for inputs and convert strings to integers
-        Write("Hello {0}! Enter the first integer: ", userName);
-        firstIntegerInputString = ReadLine();
-        firstIntegerInput = Convert.ToInt32(firstIntegerInputString);
+        // Prompt for inputs and convert strings to integers
+        Write($"Hello {userName}! Enter the first integer: ");
+        int firstIntegerInput = int.Parse(ReadLine());
 
         Write("Enter the second integer: ");
-        secondIntegerInputString = ReadLine();
-        secondIntegerInput = Convert.ToInt32(secondIntegerInputString);
+        int secondIntegerInput = int.Parse(ReadLine());
 
-        //Calculate and output result
-        sumOfInputIntegers = firstIntegerInput + secondIntegerInput;
-        WriteLine("{0}, the sum of {1} and {2} is {3}", userName, firstIntegerInput, secondIntegerInput, sumOfInputIntegers);
+        // Calculate and output result
+        int sumOfInputIntegers = firstIntegerInput + secondIntegerInput;
+        WriteLine($"{userName}, the sum of {firstIntegerInput} and {secondIntegerInput} is {sumOfInputIntegers}");
     }
 }
